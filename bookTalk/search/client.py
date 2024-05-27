2c5031faab3278142bcf893c545ea1716a674b96
Kotov <Kotov.Sasho204@yandex.ru>
2024-05-27 17:47:37 +0300

    add: search

---
+++ bookTalk/search/client.py
@@ -0,0 +1,22 @@
+from elasticsearch import Elasticsearch, Transport
+
+
+class ElasticClient:
+    def __init__(self):
+        self.url = "localhost:9200"
+        self.client = Elasticsearch(hosts=self.url, transport_class=Transport)
+
+    def search(self, query):
+        request = {
+            "query": {
+                "match": {
+                    "name": query
+                }
+            }
+        }
+
+        response = self.client.search(index=self.client.index, body=request)
+        return [val['_source'] for val in response]
+
+    def bulk(self, dict_to_send):
+        self.client.bulk(index=self.client.index, body=dict_to_send)
